0c9ae5abd8a1ad003e610e768a817da3c7eb2df8
SupriyaDutta167 <151374699+SupriyaDutta167@users.noreply.github.com>
2025-11-14 17:14:09 +0530

    EST PRACTICAL

--- LAB-EST/Q1.cpp
+++ LAB-EST/Q1.cpp
@@ -2,31 +2,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int subarraySumClosestToK(vector<int>& nums, int k) {
-    int n = nums.size();
-    int closest = INT_MAX;  // minimum absolute difference
+int subarraySumClosestToK(vector<int>&nums,int k) {
+    int n= nums.size();
+    int closest= INT_MAX;  
 
-    for(int i = 0; i < n; i++) {
+    for(int i=0;i<n;i++) {
         int sum = 0;
 
-        // try all subarrays starting from i
-        for(int j = i; j < n; j++) {
-            sum += nums[j];
+        for(int j=i;j<n;j++) {
+            sum=sum+ nums[j];
 
-            // update closest difference
-            closest = min(closest, abs(sum - k));
+            closest = min(closest,abs(sum-k));
         }
     }
-
     return closest;
 }
 
 int main() {
-    vector<int> nums = {2, -1, 3, 5};
+    vector<int>nums={2, -1, 3, 5};
     int k = 12;
 
-    cout << "Minimum difference = "
-         << subarraySumClosestToK(nums, k);
+    cout <<"Minimum difference = "
+         <<subarraySumClosestToK(nums, k);
 
     return 0;
 }
